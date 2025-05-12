fabf81800e6d7a5de19a4428f06c28daf368f235
CASIMIRO Gabrielle <gabrielle.casimiro@umr-colli-mp021.icm-institute.org>
2025-05-12 15:44:10 +0200

    Update covariate_dataframe_data_reader and add add_covariates in IndividualData

--- src/leaspy/io/data/covariate_dataframe_data_reader.py
+++ src/leaspy/io/data/covariate_dataframe_data_reader.py
@@ -1,5 +1,6 @@
 import warnings
 
+import numpy as np
 import pandas as pd
 
 from leaspy.exceptions import LeaspyDataInputError
@@ -38,6 +39,20 @@ class CovariateDataframeDataReader(AbstractDataframeDataReader):
         self.visit_reader = VisitDataframeDataReader()
 
     ######################################################
+    #               COVARIATE METHODS
+    ######################################################
+
+    @property
+    def long_outcome_names(self) -> List[FeatureType]:
+        """Name of the longitudinal outcomes in dataset"""
+        return self.visit_reader.long_outcome_names
+
+    @property
+    def n_visits(self) -> int:
+        """Number of visit in the dataset"""
+        return self.visit_reader.n_visits
+
+    ######################################################
     #               ABSTRACT METHODS IMPLEMENTED
     ######################################################
 
@@ -69,6 +84,80 @@ class CovariateDataframeDataReader(AbstractDataframeDataReader):
 
         return self.visit_reader._set_index(df)
 
+    def _clean_dataframe_covariates(
+        self, df: pd.DataFrame, *, drop_full_nan: bool, warn_empty_column: bool
+    ) -> pd.DataFrame:
+        """
+        Clean the dataframe that contains patient information
+
+        Parameters
+        ----------
+        df: pd.DataFrame
+            Dataframe with patient information
+
+        drop_full_nan: bool
+            If set to True, raw full of nan are droped
+
+        warn_empty_column: bool
+            If set to True, a warning is raise for columns full of nan
+
+
+        Returns
+        -------
+        df: pd.DataFrame
+            Dataframe with clean information
+        """
+
+        # [SPECIFIC] check_available_data
+        df_covariate = df.copy(deep=True)
+
+        # Assert covariates columns are the only one available
+        assert (df_covariate.columns == self.covariate_names).all()
+
+        # Check if there are missing values (NaN) in the covariate
+        for covariate in self.covariate_names:
+            if df_covariate[covariate].isna().any():
+                raise LeaspyDataInputError(
+                    f"Covariate '{covariate}' contains missing values (NaN)."
+                    "Please ensure that values are provided for each visit."
+                )
+
+        # Check covariate good format
+        for covariate in self.covariate_names:
+            if not np.array_equal(
+                df_covariate[covariate], df_covariate[covariate].astype(int)
+            ):
+                raise LeaspyDataInputError(
+                    f"Covariate '{covariate}' must contain only integer values."
+                )
+            df_covariate[covariate] = df_covariate[covariate].astype(int)
+
+        # Assert one unique covariate per patient and group to drop duplicates
+        if (
+            not (df_covariate.groupby("ID").nunique()[self.covariate_names].eq(1))
+            .all()
+            .all()
+        ):
+            raise LeaspyDataInputError(
+                "There must be only an unique covariate value per patient."
+            )
+        df_covariate = df_covariate.groupby("ID").first()
+
+        # Covariate must be empty to raise an error
+        if len(df_covariate) == 0:
+            raise LeaspyDataInputError("Dataframe should have at least 1 covariate")
+
+        # Assert at least 2 different values per covariate
+        for covariate in self.covariate_names:
+            n_value = df_covariate[covariate].nunique(dropna=False)
+            if n_value < 2:
+                raise LeaspyDataInputError(
+                    f"The covariate '{covariate}' has only {n_value} unique value."
+                    "Each covariate must have at least two distinct values across patients"
+                )
+
+        return df_covariate
+
     def _clean_dataframe(
         self, df: pd.DataFrame, *, drop_full_nan: bool, warn_empty_column: bool
     ) -> pd.DataFrame:
@@ -94,14 +183,52 @@ class CovariateDataframeDataReader(AbstractDataframeDataReader):
         """
 
         # Check visits
-        df_visits = self.visit_reader._clean_dataframe(
+        df_visit = self.visit_reader._clean_dataframe(
             df.drop(columns=self.covariate_names),
             drop_full_nan=drop_full_nan,
             warn_empty_column=warn_empty_column,
         )
 
-        # à compléter
+        # Check covariates
+        df_covariate = self._clean_dataframe_covariates(
+            df.reset_index()
+            .drop(self.long_outcome_names + ["TIME"], axis=1)
+            .set_index("ID"),
+            drop_full_nan=drop_full_nan,
+            warn_empty_column=warn_empty_column,
+        )
+
+        # [SPECIFIC] prepare_clean_output
+        if (
+            not df_covariate.groupby("ID")
+            .first()
+            .index.equals(df_visit.groupby("ID").first().index)
+        ):
+            raise LeaspyDataInputError(
+                "All patients must have at least one visit and one covariate"
+            )
+
+        df = df_visit.join(df_covariate)
+
         return df
+
+    def _load_individuals_data_covariates(
+        self, subj: IndividualData, df_subj: pd.DataFrame
+    ) -> None:
+        """
+        Convert information stored in a dataframe to information stored into IndividualData
+
+        Parameters
+        ----------
+        subj: IndividualData
+            One patient with her/his information, potentially empty
+
+        df_subj: pd.DataFrame
+            One patient with her/his information
+        """
+        subj.add_covariates(
+            covariates=df_subj[self.covariate_names].iloc[0].values.tolist()
+        )
 
     def _load_individuals_data(
         self, subj: IndividualData, df_subj: pd.DataFrame
@@ -118,5 +245,4 @@ class CovariateDataframeDataReader(AbstractDataframeDataReader):
             One patient with her/his information
         """
         self.visit_reader._load_individuals_data(subj, df_subj)
-        # partie covariables à changer
-        self.event_reader._load_individuals_data(subj, df_subj)
+        self._load_individuals_data_covariates(subj, df_subj)
